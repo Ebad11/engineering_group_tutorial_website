a7f774159021a29e6fcfa148e699ceb563511171
Ebad11 <ebadshk48@gmail.com>
2024-08-17 15:32:34 +0530

    added layout.js

---
+++ app/contact/[course]/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+
+export const metadata = {
+    title: "Contact Us",
+    description: "Empowering engineers through knowledge and innovation. Join our platform to excel in the field of engineering.",
+  };  
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        {children}
+        <Footer/>
+        </body>
+      
+    </html>
+  );
+}
--- app/contact/[course]/page.jsx
+++ app/contact/[course]/page.jsx
@@ -1,23 +1,19 @@
 "use client"
 import ContactForm from '@/components/forms/ContactForm';
-import Head from 'next/head';
 import { useParams } from 'next/navigation';
 import React from 'react'
 
-
+export const metadata = {
+  title: "Contact Us",
+  description: "Empowering engineers through knowledge and innovation. Join our platform to excel in the field of engineering.",
+};
 
 const page = () => {
   const {course}= useParams();
   return (
-    <>
-   <Head>
-        <title>Contact Us</title>
-        <meta name="description" content="Empowering engineers through knowledge and innovation. Join our platform to excel in the field of engineering." />
-      </Head>
     <div>
       <ContactForm course={course} />
     </div>
-    </>
   )
 }
 
