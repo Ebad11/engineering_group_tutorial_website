e4975328192daa439a733ae038e4b2aff052df9e
Ebad11 <ebadshk48@gmail.com>
2024-08-17 15:26:53 +0530

    shifted meta

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,11 @@
 import ContactForm from '@/components/forms/ContactForm';
 import React from 'react'
 
+export const metadata = {
+  title: "Contact Us",
+  description: "Empowering engineers through knowledge and innovation. Join our platform to excel in the field of engineering.",
+};
+
 const page = () => {
   return (
     <div>
