c9589deea296cb5a6b9fed396884e3249d3edc1a
Ebad11 <ebadshk48@gmail.com>
2024-08-17 15:34:03 +0530

    remved meta from [course]

--- app/contact/[course]/page.jsx
+++ app/contact/[course]/page.jsx
@@ -3,10 +3,7 @@ import ContactForm from '@/components/forms/ContactForm';
 import { useParams } from 'next/navigation';
 import React from 'react'
 
-export const metadata = {
-  title: "Contact Us",
-  description: "Empowering engineers through knowledge and innovation. Join our platform to excel in the field of engineering.",
-};
+
 
 const page = () => {
   const {course}= useParams();
