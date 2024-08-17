2ebd5b9eca31f43b4d4afeb48acfa44c629d9831
Ebad11 <ebadshk48@gmail.com>
2024-08-17 15:29:21 +0530

    added Head tag

--- app/contact/[course]/page.jsx
+++ app/contact/[course]/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 import ContactForm from '@/components/forms/ContactForm';
+import Head from 'next/head';
 import { useParams } from 'next/navigation';
 import React from 'react'
 
@@ -8,9 +9,15 @@ import React from 'react'
 const page = () => {
   const {course}= useParams();
   return (
+    <>
+   <Head>
+        <title>Contact Us</title>
+        <meta name="description" content="Empowering engineers through knowledge and innovation. Join our platform to excel in the field of engineering." />
+      </Head>
     <div>
       <ContactForm course={course} />
     </div>
+    </>
   )
 }
 
